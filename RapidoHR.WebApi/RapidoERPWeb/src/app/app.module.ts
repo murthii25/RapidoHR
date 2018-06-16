a5380cea051d903b4039f9073ec146677720b6cb
murthii25 <murthii25@outlook.com>
2018-06-16 13:05:50 +0530

    Added components for employee register

--- RapidoHR.WebApi/RapidoERPWeb/src/app/app.module.ts
+++ RapidoHR.WebApi/RapidoERPWeb/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
